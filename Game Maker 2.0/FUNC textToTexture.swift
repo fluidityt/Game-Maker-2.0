91e936e259e91cebfc255093b597715a685846fa
dudeguy <3403>
2017-01-06 05:52:11 -0500

    auto-commit hackybook January 6, 2017 5:52 AM

---
+++ Game Maker 2.0/FUNC textToTexture.swift
@@ -0,0 +1,33 @@
+//
+//  func labelToTex.swift
+//  Game Maker 2.0
+//
+//  Created by Dude Guy  on 1/4/17.
+
+import SpriteKit
+
+func textToTexture(text: String,
+                   color: SKColor = SKColor.black,
+                   bordered: Bool = true,
+                   borderColor: SKColor = SKColor.yellow) -> SKTexture {
+  
+  let returnNode = SKNode()
+  
+  let tempLabel = SKLabelNode(text: text)
+  tempLabel.fontColor = color
+  tempLabel.verticalAlignmentMode = .center
+  
+  if bordered {
+    let tempBox = SKShapeNode(rectOf: tempLabel.frame.size)
+    tempBox.strokeColor = borderColor
+  
+    tempLabel.setScale(0.9) // To make it fit inside borcer.
+    
+    tempBox.addChild(tempLabel)
+    returnNode.addChild(tempBox)
+  } else { returnNode.addChild(tempLabel) }
+  
+  if let view = g.view {
+    return view.texture(from: returnNode)!
+  } else { fatalError("ttt: view not found") }
+}
