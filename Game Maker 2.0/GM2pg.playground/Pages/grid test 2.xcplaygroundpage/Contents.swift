8f744ce2f06aee6fba198275196125f4d2764f02
dudeguy <3403>
2017-01-07 12:47:14 -0500

    auto-commit hackybook January 7, 2017 12:47 PM

--- Game Maker 2.0/GM2pg.playground/Pages/grid test 2.xcplaygroundpage/Contents.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/grid test 2.xcplaygroundpage/Contents.swift
@@ -2,11 +2,55 @@
 import SpriteKit
 import PlaygroundSupport
 
+func getMiddleNumber(number: Int) -> Int { return ((number - 1) / 2) + 1 }
+func isEven(_ amount: Int) -> Bool { if amount % 2 == 0 { return true } else { return false }}
 func buildPrompt() -> SKSpriteNode {
-  return SKSpriteNode(color: .yellow, size: CGSize(width: 25, height: 25))
+  let ret = SKSpriteNode(color: .yellow, size: CGSize(width: 25, height: 25))
+  ret.position.y -= 35
+  return ret
 }
 func buildChoice() -> SKSpriteNode {
-  return SKSpriteNode(color: .green, size: CGSize(width: 25, height: 25))
+  let ret = SKSpriteNode(color: .green, size: CGSize(width: 25, height: 25))
+  ret.position.y -= 35
+  return ret
+}
+func addChoice(to parent: SKSpriteNode) {
+  let new = buildChoice()
+  parent.addChild(new)
+  new.position.y -= 35
+  
+  var needsSpacing = false
+  if parent.children.count > 1 { needsSpacing = true }
+  
+  if needsSpacing {
+    // get maximum size of new box:
+    let size = (10 * parent.children.count) + (25 * parent.children.count)
+    
+    // build a placer node. go up to the previous choice check if it's the last index, if not, move last index over
+    
+  }
+  func alignToParent() {}   // Evens out choices under parent.
+  func spaceOut() {}        // Moves everything over to the right.
+  
+}
+func spaceOutChildren(children: [SKSpriteNode]) {
+  let finalWidth: CGFloat = {
+    let  cWidth = CGFloat (25),
+         buffer = CGFloat (10),
+         count  = CGFloat (children.count),
+    
+         fWidth = buffer * (count - 1)
+    return fWidth * (cWidth * count)
+  }()
+
+  if isEven(children.count) {
+    
+  } else {
+    
+  }
+  
+  
+  
 }
 
 let sceneView = SKView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 400, height: 450)))
@@ -20,47 +64,7 @@ LOADSCENE: do {
 
 let prompt1 = buildPrompt()
 LOADPROMPT1: do {
-  scene.addChild(prompt1)
-}
-/*
-let choice1 = buildChoice()
-LOADCHOICE: do {
-  prompt1.addChild( choice1 )
-  choice1.position.y -= 35
-}
-
-let prompt2 = buildPrompt()
-BUILDPROMPT2: do {
-  // prompt always build 1 choice;// choice always build 1 prompt
-  choice1.addChild(prompt2)
-  prompt2.position.y -= 35
-}
-*/
-
-
-
-
-func addChoice(to parent: SKSpriteNode) {
-  let new = buildChoice()
-  parent.addChild(new)
-  new.position -= 35
-  
-  var needsSpacing = false
-  if parent.children.count > 1 { needsSpacing = true }
-  
-  if needsSpacing {
-    // get maximum size of new box:
-    let size = (10 * parent.children.count) + (25 * parent.children.count)
-    
-    
-  }
-  func alignToParent() {}   // Evens out choices under parent.
-  func spaceOut() {}        // Moves everything over to the right.
-  
-}
-  // func swapChoice() {}      // Dear god...
-
-
-do {
+  scene.addChild(prompt1); prompt1.position.y += 225
+  prompt1.addChild(buildChoice())
   prompt1.addChild(buildChoice())
 }
