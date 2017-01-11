2ef966143da0709dc9e11e498a4737defb9666d9
dudeguy <3403>
2017-01-11 18:39:20 -0500

    auto-commit hackybook January 11, 2017 6:39 PM

--- Game Maker 2.0/GM2pg.playground/Pages/grid test 3.xcplaygroundpage/Contents.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/grid test 3.xcplaygroundpage/Contents.swift
@@ -14,79 +14,79 @@ extension CGRect {
 }
 
 enum util {
-
-  static func getMiddleNumber(number: Int) -> Int { return ((number - 1) / 2) + 1 }
-
-  static func isEven(_ amount: Int) -> Bool { if amount % 2 == 0 { return true } else { return false }}
-
+  
   static func buildPrompt() -> SKSpriteNode {
-
-  let ret = SKSpriteNode(color: .yellow, size: CGSize(width: 25, height: 25))
-  ret.position.y -= 35
-  return ret
-}
-
-  static func buildChoice() -> SKSpriteNode {
-  let ret = SKSpriteNode(color: .green, size: CGSize(width: 25, height: 25))
-  ret.position.y -= 35
-  return ret
-}
-
-  static func addChoice(to parent: SKSpriteNode) {
-  let new = buildChoice()
-  parent.addChild(new)
-  new.position.y -= 35
-  
-  var needsSpacing = false
-  if parent.children.count > 1 { needsSpacing = true }
-  
-  if needsSpacing {
-    // get maximum size of new box:
-    let size = (10 * parent.children.count) + (25 * parent.children.count)
     
-    // build a placer node. go up to the previous choice check if it's the last index, if not, move last index over
+    let ret = SKSpriteNode(color: .yellow, size: CGSize(width: 25, height: 25))
+    ret.position.y -= 35
+    return ret
+  }
+  
+  static func buildChoice() -> SKSpriteNode {
+    let ret = SKSpriteNode(color: .green, size: CGSize(width: 25, height: 25))
+    ret.position.y -= 35
+    return ret
+  }
+  
+  static func addChoice(to parent: SKSpriteNode) {
+    let new = buildChoice()
+    parent.addChild(new)
+    new.position.y -= 35
+    
+    var needsSpacing = false
+    if parent.children.count > 1 { needsSpacing = true }
+    
+    if needsSpacing {
+      // get maximum size of new box:
+      let size = (10 * parent.children.count) + (25 * parent.children.count)
+      
+      // build a placer node. go up to the previous choice check if it's the last index, if not, move last index over
+      
+    }
+    func alignToParent() {}   // Evens out choices under parent.
+    func spaceOut() {}        // Moves everything over to the right.
     
   }
-  func alignToParent() {}   // Evens out choices under parent.
-  func spaceOut() {}        // Moves everything over to the right.
   
-}
-
-static func spaceOutChildren(children: [SKSpriteNode]) {
-  
-  if isEven(children.count) {
-    let amountTotal = CGFloat(25 * children.count)
-    let amountPer   = CGFloat(amountTotal / CGFloat(children.count))
-    let amountStart = CGFloat(amountTotal / 2)
+  static func spaceOutChildren(children: [SKSpriteNode]) {
     
-    var counter = CGFloat(0)
-    for child in children {
-      child.position.x = (child.parent!.position.x + CGFloat(25/2)) - amountStart
-      child.position.x += amountPer * counter
-      counter += 1
-      if child == children[0] {}
-      else {
-        child.position.x -= 5
-        child.position.x += CGFloat(5 * counter)
-      }
-  
-      if child == children.last && children.count > 2 {
-        for child2 in children {
+    func getMiddleNumber(number: Int) -> Int { return ((number - 1) / 2) + 1 }
+    
+    func isEven(_ amount: Int) -> Bool { if amount % 2 == 0 { return true } else { return false }}
+    
+    if isEven(children.count) {
+      let amountTotal = CGFloat(25 * children.count)
+      let amountPer   = CGFloat(amountTotal / CGFloat(children.count))
+      let amountStart = CGFloat(amountTotal / 2)
+      
+      var counter = CGFloat(0)
+      for child in children {
+        child.position.x = (child.parent!.position.x + CGFloat(25/2)) - amountStart
+        child.position.x += amountPer * counter
+        counter += 1
+        if child == children[0] {}
+        else {
+          child.position.x -= 5
+          child.position.x += CGFloat(5 * counter)
+        }
+        
+        if child == children.last && children.count > 2 {
+          for child2 in children {
             child2.position.x -= 5
           }
+        }
       }
+    } else {
     }
-  } else {
+    if let scene = children.first?.scene {
+      //children[0].parent!.userData?.addEntries(from: <#T##[AnyHashable : Any]#>) = scene.convertPoint(toView: children[0].frame.minX)
+    }
   }
-  if let scene = children.first?.scene {
-    //children[0].parent!.userData?.addEntries(from: <#T##[AnyHashable : Any]#>) = scene.convertPoint(toView: children[0].frame.minX)
-  }
-}
-
+  
   static func getSuperParent(from node: SKSpriteNode) -> SKSpriteNode? {
-  if let superParent = node.parent?.parent as? SKSpriteNode { return superParent }
-  else { print("parent not found"); return nil }
-}
+    if let superParent = node.parent?.parent as? SKSpriteNode { return superParent }
+    else { print("parent not found"); return nil }
+  }
 }
 
 let sceneView = SKView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 400, height: 450)))
@@ -105,8 +105,7 @@ LOADPROMPT1: do {
   scene.addChild(prompt1); prompt1.position.y += 225
   prompt1.name = "First"
   
-  var countar = 0; while countar != 4 {
-    countar += 1
+  var countar = 0; while countar != 4 { countar += 1
     prompt1.addChild(util.buildChoice())
   }
   util.spaceOutChildren(children: prompt1.children as! [SKSpriteNode])
@@ -120,8 +119,9 @@ LOADPROMPT1: do {
   
   ADDCHOICES: do {
     
-    // Our bottom right prompt:d
+    // Our bottom right prompt:
     let brPrompt = prompt1.children[3].children[0] as! SKSpriteNode
+    brPrompt.name = "BottomRight"
     
     // Add and recolor children:
     var counter = 0; while counter != 8 { counter += 1 // Because I fail at simple for loops.
@@ -132,11 +132,10 @@ LOADPROMPT1: do {
     let children: [SKSpriteNode] = brPrompt.children as! [SKSpriteNode]
     for child in children { child.color = .blue }
     
-    
-    // Make sure no collisions:
+
     checkCollisions: do {
       
-      guard let sp = getSuperParent(from: brPrompt) else { fatalError("no super parent") }
+      guard let sp = util.getSuperParent(from: brPrompt) else { fatalError("no super parent") }
       
       // Our newest choices farthest left position:
       let farLeft = scene.convertPoint(toView: brPrompt.frame.centerLeft)
