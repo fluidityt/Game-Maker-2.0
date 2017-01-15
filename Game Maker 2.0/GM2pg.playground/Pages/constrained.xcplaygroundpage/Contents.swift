66b105e9c9ff80db7779ebf1a9f62a41c98f444e
dudeguy <3403>
2017-01-14 20:55:18 -0500

    auto-commit hackybook January 14, 2017 8:55 PM

--- Game Maker 2.0/GM2pg.playground/Pages/constrained.xcplaygroundpage/Contents.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/constrained.xcplaygroundpage/Contents.swift
@@ -1,7 +1,47 @@
 import SpriteKit
 import PlaygroundSupport
-  PlaygroundPage.current.liveView = Util.loadScene()
-  let scene = Util.scene, view = Util.sceneView
+
+PlaygroundPage.current.liveView = Util.loadScene()
+
+
+/// Member:
+final class Holder: SKShapeNode {
+  
+  var xPos = 0, yPos = 0
+  
+  var Items: [SKSpriteNode] = []
+  
+  func initialize() {
+    // Add constraints
+  }
+  
+  func resize() {
+    // move parent
+    // resize parent's parent holder
+  }
+  
+  func alignChoicesToParent() {
+    // Note, if a choice has a ton of shit it should be not aligned.
+    
+  }
+  
+  func getBounds() -> [CGPoint] { return [] }
+}
+
+/// Static:
+extension Holder {
+  
+  static var  holders: [Holder] = []
+  
+  static func alignHolders() {}
+
+  static func newHolder(color: SKColor = SKColor.black) -> SKShapeNode {
+   let holder = Holder(rect: CGRect(x: 0, y: 0, width: 150, height: 50), cornerRadius: 20)
+  holder.strokeColor = color
+    
+  return holder
+  }
+}
 
 final class Repeler: SKSpriteNode {
   
@@ -19,8 +59,11 @@ final class Repeler: SKSpriteNode {
   }
 }
 
-do {
-  START: do {
+/// Variables and shit:
+let scene = Util.scene, view = Util.sceneView
+
+/// Testing:
+do { START: do {
   let repel = Repeler(color: .green, size: CGSize(width: 25, height: 25))
   
   repel.initialize(scene: scene)
@@ -31,20 +74,34 @@ do {
   scene.addChild(block)
   
   let topChild  = Util.buildPrompt(); do {
-  topChild.position = block.position
-  topChild.position.y += 35
-  topChild.constraints = [SKConstraint.distance(SKRange(constantValue: 0), to: block),
-                          SKConstraint.positionY(SKRange(constantValue: topChild.position.y))]
-  scene.addChild(topChild)}
+    topChild.position = block.position
+    topChild.position.y += 35
+    topChild.constraints = [SKConstraint.distance(SKRange(constantValue: 0), to: block),
+                            SKConstraint.positionY(SKRange(constantValue: topChild.position.y))]
+    scene.addChild(topChild)}
   
   let top2Child = Util.buildPrompt(); do {
-  top2Child.position = block.position
-  top2Child.position.y += 70
-  top2Child.constraints = [SKConstraint.distance(SKRange(constantValue: 0), to: block),
-                          SKConstraint.positionY(SKRange(constantValue: top2Child.position.y))]
-  scene.addChild(top2Child)}
+    top2Child.position = block.position
+    top2Child.position.y += 70
+    top2Child.constraints = [SKConstraint.distance(SKRange(constantValue: 0), to: block),
+                             SKConstraint.positionY(SKRange(constantValue: top2Child.position.y))]
+    scene.addChild(top2Child)}
   
   repel.position = block.position
   repel.position.x += 1
+  }
+  
+  HOLDER: do {
+    let holder1x1  = Holder.newHolder(color: .yellow); do {
+      scene.addChild(holder1x1)
+    }
+    let holder2x1  = Holder.newHolder(color: .green); do {
+    holder2x1.position.x -= 190
+    holder2x1.position.y += 70
+    scene.addChild(holder2x1)}
+    let holder2x2 = Holder.newHolder(color: .greenf); do {
+      holder2x2.position.x += 35
+      holder2x2.position.y += 70
+      scene.addChild(holder2x2)}
   }
 }
