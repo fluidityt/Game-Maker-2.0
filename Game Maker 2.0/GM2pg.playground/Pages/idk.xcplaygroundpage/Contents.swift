f52ef0aa3c1cc6a6154e88db673302d66f2c405c
dudeguy <3403>
2017-01-21 20:40:45 -0500

    auto-commit hackybook January 21, 2017 8:40 PM

--- Game Maker 2.0/GM2pg.playground/Pages/idk.xcplaygroundpage/Contents.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/idk.xcplaygroundpage/Contents.swift
@@ -17,11 +17,10 @@ final class Tester: SKScene {
     box.name = "box"
     
     addChild(box)
-    box.position = toNode.position
     
     if let theParent = toNode.parent {
-      box.position = convert(toNode.position, from: theParent)
-    } else { box.position = toNode.position }
+      box.position = convert(toNode.frame.centerLeft, from: theParent)
+    } else { box.position = toNode.frame.centerLeft }
     
     return box
   }
@@ -40,90 +39,28 @@ final class Tester: SKScene {
   }
   
   override func didMove(to view: SKView) {
-  
-    /*
-    let n1 = makeSprite(color: .cyan, name: "n1", theParent: self),
-        n2 = makeSprite(color: .red, name: "n2", theParent: n1),
-        n3 = makeSprite(color: .cyan, name: "n3", theParent: n2)
-    */
     
-    // Creation:
-    let o1        = makeSprite(color: .cyan, name: "o1", theParent: self),
+    /*
+     let n1 = makeSprite(color: .cyan, name: "n1", theParent: self),
+     n2 = makeSprite(color: .red, name: "n2", theParent: n1),
+     n3 = makeSprite(color: .cyan, name: "n3", theParent: n2)
+     */
+    
+    // Creation: (take note of parents!)
+    let o1        = makeSprite(color: .cyan, name: "o1",        theParent: self),
         o2        = makeSprite(color: .red,  name: "o2",        theParent: o1),
         o3        = makeSprite(color: .cyan, name: "o3",        theParent: o2),
         o3sibling = makeSprite(color: .cyan, name: "o3sibling", theParent: o2)
-    
+        
     // Spacing:
     o1.position.x += 35
     o3.position.x += 18
     o3sibling.position.x -= 17
     
-    let box = addBox(toNode: o3sibling)
-    
-
-    print("box:", box.position)
-    print("o3sibling:", o3sibling.position)
-    print("o1:", o1.position)
-    print("o2:", o2.position)
-    
-     for node in nodes(at: box.position) {
-      print( node.name )
-    }
-    
-     //addBox(toNode: o3sibling)
-    
-    /*  addStuff: do {
-     /// Top:
-     addChild(top)
-     
-     /// TopC:
-     for child in topChildren { top.addChild(child) }
-     
-     /// Seconds:
-     var counter = -1
-     while counter != (topChildren.count - 1) {
-     counter += 1
-     topChildren[counter].addChild( second[counter] )
-     }
-     
-     /// SecondsC:
-     counter = -1
-     while counter != (second.count - 1) {
-     counter += 1
-     for child in secondChildren[counter] { second[counter].addChild(child) }
-     }
-     }*/
-    
-    shit: do {
-      let conTop1 = convert(top.position, to: self),  /// top: 0, 0  ; self: 0, -35
-      conTop2 = convert(top.position, from: self) /// top: 0, -70; self: 0, -35
+    func checkForOverlap(ofNode: SKNode) -> Bool {
+      let overlapBox = addBox(toNode: ofNode)
       
-      let conTop3 = convertPoint(toView: top.position), /// 200, 274
-      conTop4 = convertPoint(fromView: top.position)/// -160, 184
-    }
-    
-    
-  }
-  
-  private func algo() {
-    
-    var isOverlapped = true
-    
-    /// Recursive:
-    func checkIt(node: SKNode) {
-      
-      if isOverlapped { checkIt(node: node) }
-      
-    }
-    
-  }
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
-    do {
-      let touch = touches.first
-      
-      let positionInScene = touch!.location(in: self)
+      let positionInScene = overlapBox.position
       
       let allNodes = nodes(at: positionInScene)
       
@@ -134,15 +71,89 @@ final class Tester: SKScene {
         let nodeFrameConverted
           = CGRect(origin: CGPoint(x: nodePositionConverted.x - node.frame.maxX,
                                    y: nodePositionConverted.y - node.frame.maxY),
-                   size: node.frame.size)
+                                   size: node.frame.size)
         
-        if nodeFrameConverted.contains(positionInScene), let n = node.name {
-          print(n)
+        if nodeFrameConverted.contains(positionInScene) {
+          if let n = node.name { print(n) }
+        }
+      }
+      
+      return true
+    }
+    
+      //addBox(toNode: o3sibling)
+      
+      /*  addStuff: do {
+       /// Top:
+       addChild(top)
+       
+       /// TopC:
+       for child in topChildren { top.addChild(child) }
+       
+       /// Seconds:
+       var counter = -1
+       while counter != (topChildren.count - 1) {
+       counter += 1
+       topChildren[counter].addChild( second[counter] )
+       }
+       
+       /// SecondsC:
+       counter = -1
+       while counter != (second.count - 1) {
+       counter += 1
+       for child in secondChildren[counter] { second[counter].addChild(child) }
+       }
+       }*/
+      
+      shit: do {
+        let conTop1 = convert(top.position, to: self),  /// top: 0, 0  ; self: 0, -35
+        conTop2 = convert(top.position, from: self) /// top: 0, -70; self: 0, -35
+        
+        let conTop3 = convertPoint(toView: top.position), /// 200, 274
+        conTop4 = convertPoint(fromView: top.position)/// -160, 184
+      }
+      
+      
+    }
+    
+    private func algo() {
+      
+      var isOverlapped = true
+      
+      /// Recursive:
+      func checkIt(node: SKNode) {
+        
+        if isOverlapped { checkIt(node: node) }
+        
+      }
+      
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+      do {
+        let touch = touches.first
+        
+        let positionInScene = touch!.location(in: self)
+        
+        let allNodes = nodes(at: positionInScene)
+        
+        for node in allNodes {
+          
+          let nodePositionConverted = self.convert(node.position, from: node)
+          
+          let nodeFrameConverted
+            = CGRect(origin: CGPoint(x: nodePositionConverted.x - node.frame.maxX,
+                                     y: nodePositionConverted.y - node.frame.maxY),
+                     size: node.frame.size)
+          
+          if nodeFrameConverted.contains(positionInScene), let n = node.name {
+            print(n)
+          }
         }
       }
     }
   }
-}
 
 
 PlaygroundPage.current.liveView = Util.loadScene(Tester(size: CGSize(width: 320, height: 320)))
