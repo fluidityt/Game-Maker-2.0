59be80fcf1af375a9de366ba64fea12d2e7fcfa0
dudeguy <3403>
2017-01-31 09:39:08 -0500

    auto-commit hackybook January 31, 2017 9:39 AM

--- Game Maker 2.0/GM2pg.playground/Pages/constraint again.xcplaygroundpage/Sources/source.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/constraint again.xcplaygroundpage/Sources/source.swift
@@ -1,23 +1,19 @@
 import SpriteKit
 func newLine() { print("\n") }
-
-/// Classes:
+// MARK: - IGEs:
 class InGameElement: SKSpriteNode {
   final var
-  title: String,
-  childHolder: Holder
+  title = "",
+  childHolder = Holder()
   
   init(title: String, color: SKColor, size: CGSize) {
-    super.init(texture: nil, color: color, size: size)
     self.title = title
-    
+    super.init(texture: nil, color: color, size: size)
   }                                                                                 ; required init?(coder aDecoder: NSCoder) { super.init(texture: nil, color: .black, size: CGSize.zero) }
 };
 
 final class Prompt: InGameElement {
   /// Holder should be created but with a GAME OVER content
-  let childHolder = ChoiceHolder()
-  
   func childChoices() -> [Choice] {
     var choices = [Choice]()
     for child in children[0].children {
@@ -28,31 +24,27 @@ final class Prompt: InGameElement {
   
   init(title: String) {
     super.init(title: title, color: .red, size: CGSize(width: 50, height: 50))
+    childHolder = ChoiceHolder()
   }                                                                                 ; required init?(coder aDecoder: NSCoder) {super.init(coder: NSCoder())}
-  
 };
 
 final class Choice: InGameElement {
-  
-  let childHolder =
   
   func childPrompt() -> Prompt { return children[0].children[0] as! Prompt }
   
   init(title: String) {
     super.init(title: title, color: .yellow, size: CGSize(width: 100, height: 50))
+    childHolder = PromptHolder()
   }                                                                                 ; required init?(coder aDecoder: NSCoder) {super.init(coder: NSCoder())}
-  
 };
 
-
+// MARK: - Holders:
 class Holder: SKSpriteNode {
 };
 
 final class PromptHolder: Holder {
   var prompts = [Prompt]()
 };
-
-
 
 final class ChoiceHolder: Holder {
   var choices = [Choice]()
