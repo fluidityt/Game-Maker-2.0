3d674503e852c06e0339768f956ec47524da2729
dudeguy <3403>
2017-02-01 14:18:37 -0500

    auto-commit hackybook February 1, 2017 2:18 PM

--- Game Maker 2.0/GM2pg.playground/Pages/constraint again.xcplaygroundpage/Sources/test.swift
+++ Game Maker 2.0/GM2pg.playground/Pages/constraint again.xcplaygroundpage/Sources/test.swift
@@ -5,7 +5,76 @@ public func didMoveToView() {
   Sys.ineet()
   //  Sys.status()
   //  Sys.Utl.findRowTest()
-    Sys.Btn.addChoiceAndPromptTest()
+  Sys.Btn.addChoiceAndPromptTest()
+  todoComments: do {
+    // TODO: TODO:
+    /**
+     TODO:
+     - make gameover default node
+     - register shit in holders
+     */
+    
+  }
+}
+
+extension Sys.Utl {
+  static func findRowTest() {
+    /// do this first
+    
+    let superHolder = PromptHolder()
+    let superPrompt = Prompt(title: Prompt.superPromptTitle)
+    superHolder.addChild(superPrompt)
+    
+    let secondHolder = ChoiceHolder()
+    let secondChoice = Choice(title: "choicer")
+    secondHolder.addChild(secondChoice)
+    superPrompt.addChild(secondHolder)
+    
+    let secondPromptHolder = PromptHolder()
+    let secondPrompt = Prompt(title: "ss")
+    secondPromptHolder.addChild(secondPrompt)
+    secondChoice.addChild(secondPromptHolder)
+    
+    
+    print(findRow(ofIGE: secondPrompt))
+  }
+}
+
+extension Sys.Btn {
+  static func addChoiceAndPromptTest() {
+    let superHolder = PromptHolder()
+    let superPrompt = Prompt(title: Prompt.superPromptTitle)
+    superHolder.addChild(superPrompt)
+    
+    // Round 1:
+    Sys.selected = superPrompt
+    addChoice()
+    Sys.Utl.findRow(ofIGE: Sys.selected)
+    
+    // Round 2:
+    Sys.selected = superPrompt
+    addChoice()
+    Sys.Utl.findRow(ofIGE: Sys.selected)
+    
+    addPrompt()
+    Sys.Utl.findRow(ofIGE: Sys.selected)
+    
+  }
+}
+
+func oldStuff() {
+}
+/**
+ How do I register this? OH right, a search algo =/ or threaded while..
+ solutions:
+ - thread an updater that goes through and assigns a grid location
+ - check relative Y positions (no way X could work)
+ - check for number of parents (thus giving you row #)
+ - if you know the position of one thing, then the immediate things around
+ it you should know too.. such as if you add a new thing to that known
+ thing or, if you delete from that known thing--you will know where you are.
+ - cache nearby shit (scenes?)
+ */
 
 setupProcessComments: do {
   /**
@@ -32,71 +101,6 @@ setupProcessComments: do {
    - add choice holder to new prompt
    - add new prompt to choice's prompt holder
    - register in .holders
-
- How do I register this? OH right, a search algo =/ or threaded while..
-   solutions:
-   - thread an updater that goes through and assigns a grid location
-   - check relative Y positions (no way X could work)
-   - check for number of parents (thus giving you row #)
-   - if you know the position of one thing, then the immediate things around
-     it you should know too.. such as if you add a new thing to that known
-     thing or, if you delete from that known thing--you will know where you are.
-   - cache nearby shit (scenes?)
    */
-  }
-  
-  todoComments: do {
-    // TODO: TODO:
-    /**
-     TODO:
-     - make gameover default node
-     - make test for Btn.addPrompt()
-    */
-  }
 }
-
-extension Sys.Utl {
-  static func findRowTest() {
-    /// do this first
-    
-    let superHolder = PromptHolder()
-    let superPrompt = Prompt(title: Prompt.superPromptTitle)
-    superHolder.addChild(superPrompt)
-    
-    let secondHolder = ChoiceHolder()
-    let secondChoice = Choice(title: "choicer")
-    secondHolder.addChild(secondChoice)
-    superPrompt.addChild(secondHolder)
-    
-    let secondPromptHolder = PromptHolder()
-    let secondPrompt = Prompt(title: "ss")
-    secondPromptHolder.addChild(secondPrompt)
-    secondChoice.addChild(secondPromptHolder)
-    
-    
-    print(findRow(ofIGE: secondPrompt))   
-  }
 }
-
-extension Sys.Btn {
-  static func addChoiceAndPromptTest() {
-    let superHolder = PromptHolder()
-    let superPrompt = Prompt(title: Prompt.superPromptTitle)
-    superHolder.addChild(superPrompt)
-
-    // Round 1:
-    Sys.selected = superPrompt
-    addChoice()
-    Sys.Utl.findRow(ofIGE: Sys.selected)
-    
-    // Round 2:
-    Sys.selected = superPrompt
-    addChoice()
-    Sys.Utl.findRow(ofIGE: Sys.selected)
-    
-    addPrompt()
-    Sys.Utl.findRow(ofIGE: Sys.selected)
-
-  }
-}
-
